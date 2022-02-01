31c6d36248810d5590265589dba06f12a248a07c
AnthonyA <41026610+SonneyD@users.noreply.github.com>
2022-02-01 08:22:32 -0800

    first commit by aardiente

---
+++ bin/README.md
@@ -0,0 +1,7 @@
+<h1>
+	This project was created to test and demo GitHub source control 	resources.
+</h1>
+
+<h2>
+	This was created on day 2 of training
+</h2>
---
+++ src/GitMain.java
@@ -0,0 +1,12 @@
+
+public class GitMain 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("GitHub Demo :D");
+
+	}
+
+}
